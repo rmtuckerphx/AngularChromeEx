a4311ba4d126589057da2de967d859f1cbdccd65
rmtuckerphx <mark@shazaml.com>
2012-12-12 09:28:19 -0700

    Basic Chrome Extension using AngularJS and a Service

--- js/app/popup.js
+++ js/app/popup.js
@@ -1,19 +1,35 @@
-﻿myApp.controller("PageController", function ($scope) {
+﻿myApp.service('pageInfoService', function() {
+    this.getInfo = function(callback) {
+        var model = {};
+
+        chrome.tabs.query({'active': true},
+        function (tabs) {
+            if (tabs.length > 0)
+            {
+                model.title = tabs[0].title;
+                model.url = tabs[0].url;
+
+                chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
+                    model.pageInfos = response;
+                    callback(model);
+                });
+            }
+
+        });
+    };
+});
+
+myApp.controller("PageController", function ($scope, pageInfoService) {
     $scope.message = "Hello from AngularJS";
 
-    chrome.tabs.query({'active': true},
-    function (tabs) {
-        if (tabs.length > 0)
-        {
-            $scope.title = tabs[0].title;
-            $scope.url = tabs[0].url;
-
-            chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
-                $scope.pageInfos = response;
-                $scope.$apply();
-            });
-        }
+    pageInfoService.getInfo(function (info) {
+        $scope.title = info.title;
+        $scope.url = info.url;
+        $scope.pageInfos = info.pageInfos;
+        
+        $scope.$apply();
     });
 });
+
 
 
