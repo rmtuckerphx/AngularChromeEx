de7d56af9105fac0d349de31f73dd7bcebdbc4da
rmtuckerphx <mark@shazaml.com>
2012-12-12 09:20:57 -0700

    Basic Chrome Extension using AngularJS

---
+++ js/app/content.js
@@ -0,0 +1,27 @@
+﻿//alert('content script loaded');
+
+chrome.extension.onMessage.addListener(
+function (request, sender, sendResponse) {
+
+    debugger;
+
+
+    if (request.action == 'PageInfo') {
+        var pageInfos = [];
+
+        $('a').each(function() {
+            var pageInfo = {};
+            
+            var href = $(this).attr('href');
+
+            if (href != null && href.indexOf("http") == 0)
+            {
+                //only add urls that start with http
+                pageInfo.url = href
+                pageInfos.push(pageInfo);
+            }
+        });
+
+        sendResponse(pageInfos);
+    }
+});
---
+++ js/app/popup.js
@@ -0,0 +1,19 @@
+﻿myApp.controller("PageController", function ($scope) {
+    $scope.message = "Hello from AngularJS";
+
+    chrome.tabs.query({'active': true},
+    function (tabs) {
+        if (tabs.length > 0)
+        {
+            $scope.title = tabs[0].title;
+            $scope.url = tabs[0].url;
+
+            chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
+                $scope.pageInfos = response;
+                $scope.$apply();
+            });
+        }
+    });
+});
+
+
